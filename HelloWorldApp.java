c4745af43a00b8de9eeaa580132ea70c124aa765
syeung168 <syeung168@gmail.com>
2014-09-15 13:28:22 -0700

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -5,6 +5,6 @@ package com.test;
 */
 class HelloWorldApp {
 public static void main(String[] args) {
-System.out.println("Hello World!"); // Display the string.
+System.out.println("Hello World!"); 
 }
 }
